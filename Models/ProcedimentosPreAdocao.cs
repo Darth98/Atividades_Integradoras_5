850cd053b368f0fdca25b3389c99606e4feee285
Darth98 <whalyssonaguiar@hotmail.com>
2020-05-14 22:40:13 -0300

    Criacao de Controllers e Classes

---
+++ Models/ProcedimentosPreAdocao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaoLendario.Models
+{
+    public class ProcedimentosPreAdocao
+    {
+        private string descricaoProc;
+        private DateTime data;
+
+        public string DescricaoProc { get => descricaoProc; set => descricaoProc = value; }
+        public DateTime Data { get => data; set => data = value; }
+    }
+}
